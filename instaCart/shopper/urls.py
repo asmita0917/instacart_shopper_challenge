e7491fa00f5eb486c01f4d6ac3a0b2e0f3bb9227
asmita0917 <asdutta@ucsc.edu>
2017-05-04 15:46:45 -0700

    Add routes

--- instaCart/shopper/urls.py
+++ instaCart/shopper/urls.py
@@ -4,4 +4,8 @@ import views
 
 urlpatterns = [
     url(r'^shopper_home/$', views.shopper_home, name='shopper_home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'), 
+    url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
--- instaCart/shopper/views.py
+++ instaCart/shopper/views.py
@@ -13,3 +13,19 @@ def shopper_home(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def login(request):
+    html = "<html><body>It is now login page.</body></html>" 
+    return HttpResponse(html)
+
+def logout(request):
+    html = "<html><body>It is now logout page.</body></html>" 
+    return HttpResponse(html)
+
+def register(request):
+    html = "<html><body>It is now register page.</body></html>" 
+    return HttpResponse(html)
+
+def edit(request):
+    html = "<html><body>It is now edit page.</body></html>" 
+    return HttpResponse(html)
