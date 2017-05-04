028db467f442520b5e7a273663b31030e1e0d2fa
asmita0917 <asdutta@ucsc.edu>
2017-05-04 15:18:34 -0700

    Add optional command line argument for num entries

--- instaCart/shopper/management/commands/populate_db.py
+++ instaCart/shopper/management/commands/populate_db.py
@@ -1,8 +1,15 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    args = '<Foobar>'
+    args = 'number_of_entries'
     help = ''
 
+    def _create_db(self, number_of_entries):
+        print "requested number of entries %d" % number_of_entries
+
+    def add_arguments(self, parser):
+        parser.add_argument('number_of_entries', default=1000, nargs='?', type=int)
+    
     def handle(self, *args, **options):
-        print "Working"
+        number_of_entries = options['number_of_entries']
+        self._create_db(number_of_entries)
