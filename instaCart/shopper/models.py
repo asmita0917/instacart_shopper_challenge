dc5d526929193c16f6b1989e87ed247b00100cfc
asmita0917 <asdutta@ucsc.edu>
2017-05-04 15:06:59 -0700

    Added models for db

--- instaCart/shopper/models.py
+++ instaCart/shopper/models.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Applicant(models.Model):
+
+    id = models.AutoField(serialize=False, primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    application_date = models.DateField(db_index=True, default=timezone.now)
+    workflow_state = models.CharField(max_length=100, default='applied')
+    created_at = models.DateField(default=timezone.now)
+    updated_at = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return '%s %s %s %s %s' % (self.id, self.name, self.email, self.workflow_state, self.application_date)
